7e3cb025b3e262377eaafb9a9b43264e45e268fd
Patricia DSouza <patsy@Patricias-MacBook-Air.local>
2020-04-29 17:33:06 -0400

    Please check the Bean

--- sso/src/main/java/com/freedommortgage/sso/SsoApplication.java
+++ sso/src/main/java/com/freedommortgage/sso/SsoApplication.java
@@ -1,9 +1,14 @@
 package com.freedommortgage.sso;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@EntityScan(basePackages = "com.freedommortgage.sso")
+@ComponentScan(basePackages = {"com.freedommortgage.sso"})
+@EnableAutoConfiguration
 public class SsoApplication {
 
 	public static void main(String[] args) {
